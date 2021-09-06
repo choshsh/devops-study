035bae8f3461b012a49b18a259cc58d276c5a518
Sang-hyun, Cho <cho911115@gmail.com>
2021-09-06 21:25:20 +0900

    terraform - aws vpc 중간완료 #7
    기본적인 구성 완료.
    보안 정책은 고민 필요

--- learn-terraform/eks/outputs.tf
+++
@@ -1,9 +0,0 @@
-output "endpoint" {
-  value = aws_eks_cluster.my-eks-cluster.endpoint
-}
-output "kubeconfig-certificate-authority-data" {
-  value = aws_eks_cluster.my-eks-cluster.certificate_authority[0].data
-}
-output "eks_cluster_id" {
-  value = aws_eks_cluster.my-eks-cluster.id
-}
--- terraform/vpc/variables.tf
+++ terraform/vpc/variables.tf
@@ -1,4 +1,4 @@
 variable "workspace" {
-  type        = string
   description = "Terraform workspace"
+  type        = string
 }
