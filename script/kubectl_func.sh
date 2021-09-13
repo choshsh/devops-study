662e0b4a7a11ecad974b27d5eeafb324c17025bc
Sang-hyun, Cho <cho911115@gmail.com>
2021-09-13 19:37:18 +0900

    Edit

--- script/kubectl_func.sh
+++ script/kubectl_func.sh
@@ -5,17 +5,18 @@
 # @param NAME       resource name
 # @param REPLICAS   scale replicas count
 
-function scale(){
-  kubectl scale deployment $NAME --replicas=$REPLICAS -n $NAMESPACE
-}
+# function scale(){
+#   kubectl scale deployment $NAME --replicas=$REPLICAS -n $NAMESPACE
+# }
 
 function get_all(){
   kubectl get all -n $NAMESPACE
 }
 
-function get_istio(){
-  kubectl get vs,dr $NAME -n $NAMESPACE
-}
+# function get_istio(){
+#   kubectl get vs,dr $NAME -n $NAMESPACE
+# }
+
 if [ -z "$1" ]; then
   echo "Parameter is not valid."
 else
