29a69ff50df730084d7c1004199e8af95b75df8a
Sang-hyun, Cho <cho911115@gmail.com>
2021-09-18 00:29:54 +0900

    Dev (#15)
    * Edit Istio - ArgoCD
    
    * Test using kubectl by jenkins job
    
    * Init kaniko #12
    
    * Kaniko dockerhub 인증 오류 때문에 secret endpoint v1로 변경 #12
    
    * jenkins kubectl 테스트
    
    * Using Kubernetes NFS Subdir External Provisioner
    
    * Edit terraform #7, #8
    
    * Kaniko image build by jenkins CI #12
    
    * Git 이름 변경
    
    * 타임아웃 추가 #12
    
    * Add loki-stack
    
    metric, logging 모니터링을 위한 loki, prometheus, promtail 등이 포함된다
    
    * Add bash apk
    
    * Add DNS debug pipeline

--- script/kubectl_func.sh
+++ script/kubectl_func.sh
@@ -5,20 +5,30 @@
 # @param NAME       resource name
 # @param REPLICAS   scale replicas count
 
-# function scale(){
-#   kubectl scale deployment $NAME --replicas=$REPLICAS -n $NAMESPACE
-# }
-
-function get_all(){
-  kubectl get all -n $NAMESPACE
+function scale(){
+  kubectl scale deployment $NAME --replicas=$REPLICAS -n $NAMESPACE
 }
 
-# function get_istio(){
-#   kubectl get vs,dr $NAME -n $NAMESPACE
-# }
+function get_resource(){
+  if [ -z "$NAME" ]; then
+    kubectl get $RESOURCE -n $NAMESPACE
+  else
+    kubectl get $RESOURCE $NAME -n $NAMESPACE
+  fi
+}
+
+function get_istio(){
+  if [ -z "$NAME" ]; then
+    kubectl get vs,dr -n $NAMESPACE
+  else
+    kubectl get vs, dr $NAME -n $NAMESPACE
+  fi
+}
 
 if [ -z "$1" ]; then
-  echo "Parameter is not valid."
+  echo "Please input function name."
 else
   $1
 fi
+
+exit 0
