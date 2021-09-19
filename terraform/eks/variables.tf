d5f8c82ce242fdb34f872a8cd879c61708ce7241
Sang-hyun, Cho <cho911115@gmail.com>
2021-09-20 02:21:46 +0900

    terraform variable 수정 (eks, eks node group, fargate)

--- terraform/eks/variables.tf
+++ terraform/eks/variables.tf
@@ -12,3 +12,13 @@ variable "private_subnet_ids" {
   description = "Subnet IDs"
   type        = list(string)
 }
+
+variable "eks_node_group" {
+  description = "Node-group config"
+  type        = map(string)
+}
+
+variable "fargate_profiles" {
+  description = "Fargate profiles"
+  type        = list(any)
+}
