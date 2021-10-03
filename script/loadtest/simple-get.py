a311bdf51d14bd26ddfee4d9745aaffa0f3809b1
Sang-hyun, Cho <cho911115@gmail.com>
2021-10-04 00:28:37 +0900

    Feature load test (#46)
    * Update

--- script/loadtest/simple-get.py
+++ script/loadtest/simple-get.py
@@ -1,8 +1,8 @@
 from locust import HttpUser, task
 
 
-class HelloWorldUser(HttpUser):
+class SimpleGetUser(HttpUser):
+
     @task
     def hello_world(self):
-        self.client.verify = False
         self.client.get("/")
