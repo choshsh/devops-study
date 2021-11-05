ccfef1eab91e3b20b9ef31159fb2469f65a034ad
Sang-hyun, Cho <cho911115@gmail.com>
2021-11-06 01:46:01 +0900

    kube-proxy ipvs 모드 사용을 위한 설정 추가
    - ipvs 관련 커널 모듈 로드
    - ipvsadm, ipset, iproute-tc 패키지 설치
    - 커널 업데이트 반영되도록 reboot 추가

--- script/install_k8s.sh
+++ script/install_k8s.sh
@@ -1,17 +1,28 @@
 #! /bin/bash
 
 # Kubernetes version
-KUBE_VERSION="1.22.2"
+KUBE_VERSION="1.22.3"
 
 # Install kubernetes on linux dist from Red Hat
 # Use containerd as CRI
 function pre_config(){
     sudo sed -i "s/127.0.0.1/127.0.0.1 $HOSTNAME/" /etc/hosts
-    sudo yum install -y iproute-tc
+    sudo yum update -y
+    # setting for using kube-proxy ipvs mode
+    sudo yum install -y iproute-tc ipset ipvsadm
+    cat <<EOF | sudo tee /etc/sysconfig/modules/ipvs.modules
+#!/bin/bash
+modprobe -- ip_vs
+modprobe -- ip_vs_rr
+modprobe -- ip_vs_wrr
+modprobe -- ip_vs_sh
+modprobe -- nf_conntrack_ipv4
+EOF
+    chmod 755 /etc/sysconfig/modules/ipvs.modules && bash /etc/sysconfig/modules/ipvs.modules
 
     if [ $(grep '^NAME' /etc/os-release | grep -i centos | wc -l) -gt 0 ]; then
-        sudo swapoff -a && \
-            sudo echo 0 > /proc/sys/vm/swappines
+        sudo swapoff -a
+        sudo echo 0 > /proc/sys/vm/swappines
         sudo sed -e '/swap/ s/^#*/#/' -i /etc/fstab;
         sudo systemctl disable --now firewalld
     fi
@@ -93,6 +104,6 @@ function main(){
     install_k8s
 }
 
-main
+main && reboot
 
 exit 0
